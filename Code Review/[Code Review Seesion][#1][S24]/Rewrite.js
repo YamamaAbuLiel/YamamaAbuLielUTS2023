c861e733997c78b8ded4690c04e3f3a953ac2c95
YamamaAbuLiel <124617912+YamamaAbuLiel@users.noreply.github.com>
2023-07-29 23:52:16 +0300

    Task24

---
+++ Code Review/[Code Review Seesion][#1][S24]/Rewrite.js
@@ -0,0 +1,17 @@
+function foodsAndDrinks(){
+	const drinks = ['lemonade', 'soda', 'tea', 'water'];
+	const food = ['beans', 'chicken', 'rice'];
+
+	
+    for (var i = 0; i < drinks.length; i++) {
+        console.log(drinks[i]);
+      }
+
+      for (var i = 0; i < food.length; i++) {
+        console.log(food[i]);
+      }
+    
+
+
+
+    }
